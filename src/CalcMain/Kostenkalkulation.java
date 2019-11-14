ed76c69b4b634df9d4453cc9e33473cba87691e9
TST <TST@super.com>
2019-11-14 20:59:03 +0100

    added access to DB

--- src/CalcMain/Kostenkalkulation.java
+++ src/CalcMain/Kostenkalkulation.java
@@ -1,11 +1,9 @@
-/**
-  *
-  * Beschreibung
-  *
-  * @version 1.0 vom 12.11.2019
-  * @author 
-  */
 package CalcMain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Kostenkalkulation {
@@ -16,16 +14,40 @@ public class Kostenkalkulation {
     double properson = 0;
     int programmstatus = 0;
     int abfrage = 0;
+    String sqlQuery = "";
+    String text1 = "select * from raeume where ID = ";
     Scanner tastatur = new Scanner(System.in);
     
     while (programmstatus <3) {
-      try{         
+      try{
+    	  Class.forName("com.mysql.jdbc.Driver");  
+    		Connection con=DriverManager.getConnection(  
+    		"jdbc:mysql://localhost:3306/kostenkalkulation","root","");  
+    		//here sonoo is database name, root is username and password  
+    		Statement stmt=con.createStatement();  
+    		//ResultSet rs=stmt.executeQuery("select * from raeume");  
+    		//while(rs.next())  
+    		//System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+    		//con.close();
         switch (programmstatus) {
           case 0 : 
-            System.out.println("Bitte gib die Fixkosten ein.");
-            fixKosten = tastatur.nextDouble();
+            System.out.println("Bitte wähle die Fixkosten aus.");
+            //Auslesen der Tabelleneinträge
+    		ResultSet rs=stmt.executeQuery("select * from raeume");  
+    		while(rs.next())  
+    		System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getDouble(3));
+    		//Auswahl der Fixkosten
+    		abfrage = tastatur.nextInt();
+    		//Abrufen der Fixkosten aus der Datenbank
+    		sqlQuery = text1 + abfrage + ";";
+    		System.out.println(sqlQuery);
+    		ResultSet rs2=stmt.executeQuery(sqlQuery);
+    		while(rs2.next()) {			
+    			fixKosten = rs2.getDouble(3);
+    		}
             programmstatus = 1;
             break;
+    
           case 1 : 
             System.out.println("Bitte gib die variablen Kosten (pro Person) ein.");
             varKosten = tastatur.nextDouble();
@@ -71,10 +93,14 @@ public class Kostenkalkulation {
         } 
         if (personen == 0) {
           System.out.println("Division durch 0. Die Anzahl der Personen darf nicht null betragen");
-        }       
+          }
+          else {
+          }
+        	  
+          }
+        catch(Exception e){
+    	   System.out.println(e);
+       }        
       }
     } // end of while
   } // end of main
-    
-} // end of class Kostenkalkulation
-              
